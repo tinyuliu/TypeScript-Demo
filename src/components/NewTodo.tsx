dad0678a30036e567965291975cc8d939b9a39b5
tinyuliu <tinyu1016@gmail.com>
2021-07-29 19:36:26 +0800

    add form submission

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,11 @@
+const NewTodo = () => {
+    return (
+        <form>
+            <label htmlFor='text'>Todo texet</label>
+            <input type='text' id='text' />
+            <button>Add Todo</button>
+        </form>
+    )
+};
+
+export default NewTodo;

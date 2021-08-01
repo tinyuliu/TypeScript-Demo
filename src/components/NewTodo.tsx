a25e15ef86bdee07ae34e98b108e44631f758733
tinyuliu <tinyu1016@gmail.com>
2021-08-01 19:12:10 +0800

    context APi

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,8 +1,12 @@
-import React, { useRef } from "react";
+import React, { useRef, useContext } from "react";
 
+import { TodosContext } from "../store/todos-context";
 import classes from './NewTodo.module.css';
 
-const NewTodo: React.FC<{ onAddTodo: (text: string)=> void }> = (props) => {
+const NewTodo: React.FC = () => {
+
+    const todosCtx = useContext(TodosContext);
+
     const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event: React.FormEvent) => {
@@ -11,11 +15,10 @@ const NewTodo: React.FC<{ onAddTodo: (text: string)=> void }> = (props) => {
         const enteredText = todoTextInputRef.current!.value;
 
         if (enteredText.trim().length === 0){
-            // return an error
             return;
         }
         
-        props.onAddTodo(enteredText);
+        todosCtx.addTodo(enteredText);
 
     };
 
